ae9b1f9a20fb9b9987e78041f91ff64e8f1d1e71
Abdalkabar <aabolghed@gmail.com>
2026-06-24 16:58:11 +0300

    Add files via upload
    This is part of the webapplication1project

--- Program.cs
+++ Program.cs
@@ -1,12 +1,7 @@
-using Microsoft.EntityFrameworkCore;
-using TechStoreInventory.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<InventoryContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("InventoryManager")));
 
 var app = builder.Build();
 
